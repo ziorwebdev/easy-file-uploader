808c9ac410e06caa7d19ec2d87de41dcbe1d96be
Rey C <rey@usarus.co.nz>
2025-03-13 19:46:44 +0800

    Update class-assets.php

--- includes/classes/class-assets.php
+++ includes/classes/class-assets.php
@@ -18,6 +18,21 @@ class Assets {
 	 */
 	private static ?Assets $instance = null;
 
+	private function enqueue_wp_filepond_scripts(): void {
+		$uploader_configurations = get_uploader_configurations();
+
+		wp_enqueue_style( 'wp-filepond-vendors', WP_FILEPOND_PLUGIN_URL . 'dist/vendors.min.css', array(), null );
+		wp_enqueue_script( 'wp-filepond-vendors', WP_FILEPOND_PLUGIN_URL . 'dist/vendors.min.js', array(), null, true );
+
+		// Allow other addon plugins to enqueue their own scripts and styles.
+		do_action( 'enqueue_wp_filepond_scripts' );
+
+		wp_enqueue_style( 'wp-filepond-uploader', WP_FILEPOND_PLUGIN_URL . 'dist/main.min.css', array(), null );
+		wp_enqueue_script( 'wp-filepond-uploader', WP_FILEPOND_PLUGIN_URL . 'dist/main.min.js', array( 'jquery' ), null, true );
+
+		wp_localize_script( 'wp-filepond-uploader', 'FilePondUploader', $uploader_configurations );
+	}
+
 	/**
 	 * Constructor.
 	 *
@@ -31,21 +46,21 @@ class Assets {
 	/**
 	 * Enqueues scripts and styles for the admin area.
 	 */
-	public function enqueue_admin_scripts(): void {}
+	public function enqueue_admin_scripts(): void {
+		$this->enqueue_wp_filepond_scripts();
+
+		// Allow other addon plugins to enqueue their own scripts and styles.
+		do_action( 'enqueue_wp_filepond_admin_scripts' );
+	}
 
 	/**
 	 * Enqueues scripts and styles for the front-end.
 	 */
 	public function enqueue_frontend_scripts(): void {
-		$uploader_configurations = get_uploader_configurations();
+		$this->enqueue_wp_filepond_scripts();
 
-		wp_enqueue_style( 'wp-filepond-vendors', WP_FILEPOND_PLUGIN_URL . 'dist/vendors.min.css', array(), null );
-		wp_enqueue_script( 'wp-filepond-vendors', WP_FILEPOND_PLUGIN_URL . 'dist/vendors.min.js', array(), null, true );
-
-		wp_enqueue_style( 'wp-filepond-uploader', WP_FILEPOND_PLUGIN_URL . 'dist/main.min.css', array(), null );
-		wp_enqueue_script( 'wp-filepond-uploader', WP_FILEPOND_PLUGIN_URL . 'dist/main.min.js', array( 'jquery' ), null, true );
-
-		wp_localize_script( 'wp-filepond-uploader', 'FilePondUploader', $uploader_configurations );
+		// Allow other addon plugins to enqueue their own scripts and styles.
+		do_action( 'enqueue_wp_filepond_frontend_scripts' );
 	}
 
 	/**
