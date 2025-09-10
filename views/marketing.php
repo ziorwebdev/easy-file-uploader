375753ad7b010c532ad397ea8399408b020adce4
Rey C <rey@usarus.co.nz>
2025-09-11 01:04:12 +0800

    Update marketing.php

--- views/marketing.php
+++ views/marketing.php
@@ -15,11 +15,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<h4>Additional Features</h4>
 	<ul>
 		<li><span>✔</span> Sync uploaded files to media library</li>
+		<li><span>✔</span> Attach Uploaded Files to Emails</li>
 		<li><span>✔</span> HEIC/HEIF image support</li>
 		<li><span>✔</span> Image preview</li>
 		<li><span>✔</span> Custom upload location</li>
 		<li><span>✔</span> One (1) year support and updates</li>
 	</ul>
 	<p class="pricing">For as low as <strong>$39.90 / year</strong></p>
-	<a href="https://my.pluggable.io/plugin/filepond-wp-integration-pro/" target="_blank" class="button-primary">Get Pro</a>
+	<a href="https://pluggable.io/plugin/filepond-wp-integration-pro/" target="_blank" class="button-primary">Get Pro</a>
 </div>
