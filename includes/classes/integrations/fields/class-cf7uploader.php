989c9bbea9b1f2cbc1318fdf66c258ba9a6cbe6e
Rey C <rey@usarus.co.nz>
2025-09-10 23:06:00 +0800

    Code cleanup

--- includes/classes/integrations/fields/class-cf7uploader.php
+++ includes/classes/integrations/fields/class-cf7uploader.php
@@ -95,7 +95,6 @@ class CF7Uploader {
 	 *
 	 * @since 1.0.0
 	 * @param array  $posted_data The posted form submission data to be processed.
-	 * @param string $value_orig  The raw field value before any sanitization or transformation.
 	 * @return array The processed posted data.
 	 */
 	public function set_posted_data( array $posted_data ): array {
